ee186a202eed6b7c53b103f5ae3e670660cedc2a
s1f10210368 <s1f102103684@iniad.org>
2023-11-21 14:22:51 +0900

    treasurebox

--- src/main.rs
+++ src/main.rs
@@ -1,90 +1,115 @@
-use bevy::prelude::*;
+use bevy::{ prelude::*, render::*, render::settings::* };
+use std::f32::consts::*;
 
 fn main()
-{   App::new()
-        .add_plugins( DefaultPlugins ) //各種の面倒を見てもらう
+{   //Note：手元の環境だとVulkanのままでは影が描画されなかったので、DX12へ切り替えた。
+    let backends = Some ( Backends::DX12 );
+    let wgpu_settings = WgpuSettings { backends, ..default() };
+    let backend_dx12 = RenderPlugin { wgpu_settings };
+
+    App::new()
+        .add_plugins
+        (   DefaultPlugins //各種の面倒を見てもらう
+                /*.set( backend_dx12 )*/ //Note：この行をコメントアウトするとデフォルトになる
+        )
         .add_systems
         (   Startup, 
-            (   spawn_camera2d,          //カメラを作る
-                spawn_text2d_helloworld, //2Dテキストを作る
+            (   spawn_camera3d_and_light, //カメラを作る
+                spawn_3d_lockedchest,     //3Dオブジェクトを作る
             )
         )
         .add_systems
         (   Update,
             (   bevy::window::close_on_esc, //[ESC]キーで終了
-                change_color_helloworld,    //文字の色を変える
-                change_size_helloworld,     //文字の大きさを変える
             )
         )
         .run();
 }
 
-//カメラを作る
-fn spawn_camera2d( mut cmds: Commands )
-{   cmds.spawn( Camera2dBundle::default() );
+//カメラと光源を作る
+fn spawn_camera3d_and_light( mut cmds: Commands )
+{   //3Dカメラ
+    cmds.spawn( Camera3dBundle::default() )
+        .insert
+        (   Transform::from_translation( Vec3::new( -1.0, 1.0, 2.0 ) ) //カメラの位置
+                .looking_at( Vec3::ZERO, Vec3::Y ) //カメラレンズの向き
+        );
+
+    //光源
+    cmds.spawn( DirectionalLightBundle::default() )
+        .insert
+        (   DirectionalLight
+            {   illuminance: 15000.0,  //明るさ
+                shadows_enabled: true, //影の描画を有効化
+                ..default()
+            }
+        )
+        .insert
+        (   Transform::from_xyz( 30.0, 100.0, 40.0 ) //光源の位置
+                .looking_at( Vec3::ZERO, Vec3::Z )   //光源の向き
+        );
 }
 
-//マーカーの準備
-#[derive( Component )] struct HelloWorld;
-
-//2Dテキストを作る
-fn spawn_text2d_helloworld( mut cmds: Commands )
-{   //"Hello, world!"を１文字ごとに分割
-    let mut sections = Vec::new();
-    for char in "Hello, world!".chars()
-    {   let value = char.to_string();
-        let style = TextStyle { font_size: 100.0, ..default() };
-        sections.push( TextSection { value, style } );
-    }
-    let text = Text { sections, ..default() };
-
-    cmds.spawn( ( Text2dBundle { text, ..default() }, HelloWorld ) );
-}
-
-//文字の色を変える
-fn change_color_helloworld
-(   mut q_text: Query<&mut Text, With<HelloWorld>>,
-    time: Res<Time>,
-    mut angle: Local<f32>,
+//3Dオブジェクトを作る
+fn spawn_3d_lockedchest
+(   mut cmds: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
 )
-{   let Ok ( mut text ) = q_text.get_single_mut() else { return };
+{   //地面
+    cmds.spawn( PbrBundle::default() )
+        .insert( meshes.add( shape::Plane::from_size( 2.0 ).into() ) )
+        .insert( Transform::from_translation( Vec3::ZERO ) )
+        .insert( materials.add( Color::rgb( 0.5, 0.7, 0.3 ).into() ) );
 
-    let time_delta = time.delta().as_secs_f32(); //前回の実行からの経過時間
-    *angle += 360.0 * time_delta;
-    *angle -= if *angle > 360.0 { 360.0 } else { 0.0 };
+    //宝箱
+    cmds.spawn( PbrBundle::default() )
+        .insert( materials.add( Color::NONE.into() ) ) //透明
+        .insert( Transform::from_translation( Vec3::new( 0.0, 0.5, 0.0 ) ) )
+        .with_children
+        (   | cmds |
+            {   //本体
+                let shape_box = shape::Box::new( 0.7, 0.3, 0.4 );
+                cmds.spawn( PbrBundle::default() )
+                    .insert( meshes.add( shape_box.into() ) )
+                    .insert( Transform::from_translation( Vec3::Y * -0.35 ) )
+                    .insert( materials.add( Color::MAROON.into() ) );
 
-    //text.sectionsをイテレーターで回して、文字ごとに色を変える
-    for ( i, char ) in text.sections.iter_mut().enumerate()
-    {   //hue(色相)
-        let mut hue = *angle + 10.0 * i as f32;
-        hue -= if hue > 360.0 { 360.0 } else { 0.0 };
+                //上蓋
+                let shape_cylinder = shape::Cylinder { height: 0.695, radius: 0.195, ..default() };
+                cmds.spawn( PbrBundle::default() )
+                    .insert( meshes.add( shape_cylinder.into() ) )
+                    .insert
+                    (   Transform::from_translation( Vec3::Y * -0.2 )
+                            .with_rotation( Quat::from_rotation_z( PI * 0.5 ) )
+                    )
+                    .insert( materials.add( Color::MAROON.into() ) );
 
-        //文字の色を変更
-        char.style.color = Color::hsl( hue, 1.0, 0.5 );
-    } 
-}
+                //錠前
+                let shape_cube = shape::Cube::new( 0.1 );
+                cmds.spawn( PbrBundle::default() )
+                    .insert( meshes.add( shape_cube.into() ) )
+                    .insert( Transform::from_translation( Vec3::Y * -0.2 + Vec3::Z * 0.17 ) )
+                    .insert( materials.add( Color::GRAY.into() ) )
+                    .with_children
+                    (   | cmds |
+                        {   //鍵穴
+                            let cylinder = shape::Cylinder { height: 0.11, radius: 0.01, ..default() };
+                            cmds.spawn( PbrBundle::default() )
+                                .insert( meshes.add( cylinder.into() ) )
+                                .insert
+                                (   Transform::from_translation( Vec3::Y * 0.02 )
+                                        .with_rotation( Quat::from_rotation_x( PI * 0.5 ) )
+                                )
+                                .insert( materials.add( Color::BLACK.into() ) );
 
-//文字の大きさを変える
-fn change_size_helloworld
-(   mut q_text: Query<&mut Text, With<HelloWorld>>,
-    time: Res<Time>,
-    mut angle: Local<f32>,
-)
-{   let Ok ( mut text ) = q_text.get_single_mut() else { return };
-
-    let time_delta = time.delta().as_secs_f32(); //前回の実行からの経過時間
-    *angle += 360.0 * time_delta;
-    *angle -= if *angle > 360.0 { 360.0 } else { 0.0 };
-
-    //text.sectionsをイテレーターで回して、文字ごとに大きさを変える
-    for ( i, char ) in text.sections.iter_mut().enumerate()
-    {   //sin()を使って文字の大きさを伸縮させる
-        let mut angle = *angle + 10.0 * i as f32;
-        angle -= if angle > 360.0 { 360.0 } else { 0.0 };
-        let size = ( 2.0 + angle.to_radians().sin() ) * 50.0;
-
-        //文字の大きさを変更
-        //Note：小数点以下を処理しないと実行時にメモリフットプリントが爆発する
-        char.style.font_size = ( size * 10.0 ).floor() / 10.0;
-    }
+                            let shape_box = shape::Box::new( 0.01, 0.04, 0.11 );
+                            cmds.spawn( PbrBundle::default() )
+                                .insert( meshes.add( shape_box.into() ) )
+                                .insert( Transform::from_translation( Vec3::Y * 0.0 ) )
+                                .insert( materials.add( Color::BLACK.into() ) );
+                        }
+                    );
+            }
+        );
 }
